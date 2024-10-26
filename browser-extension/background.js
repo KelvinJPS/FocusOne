aa524d66784b384ecd90ac0a768f899586c3a6fd
KelvinJPS <kelvinjps@proton.me>
2024-10-25 19:58:15 -0500

    change extension to firefox/general

--- browser-extension/background.js
+++ browser-extension/background.js
@@ -1,46 +1,72 @@
-let allowedWebsites = [];
+/*
+ */
+let port = browser.runtime.connectNative("focusone");
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === 'setAllowedWebsites') {
-    allowedWebsites = message.websites;
-    updateBlockingRules();
-  }
+/*
+Listen for messages from the app.
+*/
+port.onMessage.addListener((response) => {
+  console.log(`Received: ${response}`);
 });
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: [1],
-    addRules: [{
-      id: 1,
-      priority: 1,
-      action: { type: 'block' },
-      condition: { urlFilter: '*://*/*', resourceTypes: ['main_frame'] }
-    }]
-  });
+/*
+On a click on the browser action, send the app a message.
+*/
+browser.browserAction.onClicked.addListener(() => {
+  console.log("Sending:  ping");
+  port.postMessage("ping");
 });
 
-function updateBlockingRules() {
-  const rules = [{
-    id: 1,
-    priority: 1,
-    action: { type: 'block' },
-    condition: {
-      urlFilter: '*://*/*',
-      resourceTypes: ['main_frame'],
-      excludedRequestDomains: allowedWebsites
-    }
-  }];
-
-  chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: [1],
-    addRules: rules
-  });
-}
-
-chrome.runtime.onMessageExternal.addListener((message, sender, sendResponse) => {
-  if (message.allowed_websites) {
-    allowedWebsites = message.allowed_websites;
-    updateBlockingRules();
-    sendResponse({ success: true });
-  }
-});
+// /*blocking functionality/
+// let allowedWebsites = [];
+//
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.type === "setAllowedWebsites") {
+//     allowedWebsites = message.websites;
+//     updateBlockingRules();
+//   }
+// });
+//
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.declarativeNetRequest.updateDynamicRules({
+//     removeRuleIds: [1],
+//     addRules: [
+//       {
+//         id: 1,
+//         priority: 1,
+//         action: { type: "block" },
+//         condition: { urlFilter: "*://*/*", resourceTypes: ["main_frame"] },
+//       },
+//     ],
+//   });
+// });
+//
+// function updateBlockingRules() {
+//   const rules = [
+//     {
+//       id: 1,
+//       priority: 1,
+//       action: { type: "block" },
+//       condition: {
+//         urlFilter: "*://*/*",
+//         resourceTypes: ["main_frame"],
+//         excludedRequestDomains: allowedWebsites,
+//       },
+//     },
+//   ];
+//
+//   chrome.declarativeNetRequest.updateDynamicRules({
+//     removeRuleIds: [1],
+//     addRules: rules,
+//   });
+// }
+//
+// chrome.runtime.onMessageExternal.addListener(
+//   (message, sender, sendResponse) => {
+//     if (message.allowed_websites) {
+//       allowedWebsites = message.allowed_websites;
+//       updateBlockingRules();
+//       sendResponse({ success: true });
+//     }
+//   },
+// );
